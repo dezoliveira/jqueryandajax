919cbe6089612b2364a7c06e7129e0ba51cf627d
dezoliveira <andresoliveira@protonmail.com>
2021-09-29 08:48:00 -0300

    Adicionado bootstrap

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
 function consultaCep(){
-  let cep = document.getElementById("cep").value;
-  let url = "https://viacep.com.br/ws/" + cep + "/json/"
+  let textCep = document.getElementById("text-cep").value;
+  let url = "https://viacep.com.br/ws/" + textCep + "/json/"
   console.log(url)
   $.ajax({
     url: url,
     type: "GET",
     success: function(res){
       console.log(res)
-/*       console.log(res)
       $('#cep').html(res.cep);
       $('#logradouro').html(res.logradouro);
       $('#bairro').html(res.bairro);
       $('#localidade').html(res.localidade);
-      $('#uf').html(res.uf); */
-
+      $('#uf').html(res.uf);
     }
   })
 }
+
+$(function(){
+  $(".hide-data").hide();
+});
